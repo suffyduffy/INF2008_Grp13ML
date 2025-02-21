7a384aadebfb89554a917be7be7a7396a7334e8e
suffyduffy <elfiansufi@gmail.com>
2025-02-21 21:23:59 +0800

    Completed DecisionTree and performing well with graph included

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,11 +1,9 @@
-# Import necessary libraries
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import r2_score
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 # Load the cleaned dataset
 df = pd.read_csv("cleanedHDB.csv")
