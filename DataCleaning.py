772b4eefca7ca42e30e342355f860a39dc67ba9e
Suffyduffy <elfiansufi@gmail.com>
2025-02-25 16:20:32 +0800

    Hamizi CHanges to dataCleaning

--- DataCleaning.py
+++ DataCleaning.py
@@ -5,5 +5,7 @@ df = pd.read_csv('HDBData.csv')
 df = df.dropna() #this part to remove empty cells
 df = df.drop_duplicates() #this part to remove duplicates
 
+df = df[df['flat_type'] != 'MULTI-GENERATION']
+
 print("It worked lmao")
 df.to_csv('cleanedHDB.csv', index=False)
